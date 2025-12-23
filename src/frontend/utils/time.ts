eada0199fa1a8800134c2116af6f8f99340e81d5
lobster <lobster@pobox.com>
2025-12-23 12:58:43 +0100

    No need to format a time string with seconds

--- src/frontend/utils/time.ts
+++ src/frontend/utils/time.ts
@@ -1,10 +1,9 @@
 /**
- * Converts a number of seconds into a HH:MM:SS string.
+ * Converts a number of seconds into a HH:MM string.
  */
 export const formatSeconds = (totalSeconds: number): string => {
     if (totalSeconds < 0) totalSeconds = 0;
     const hours = Math.floor(totalSeconds / 3600);
     const minutes = Math.floor((totalSeconds % 3600) / 60);
-    const seconds = Math.floor(totalSeconds % 60);
-    return `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+    return `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}`;
 };
